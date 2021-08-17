f17d1038a05ab9445fb992e7a28a00febb069dc8
Андрей Колчин <nakaz@bk.ru>
2021-08-17 18:14:57 +0300

    Добавлены файлы с решением домашнего задания

---
+++ task_1_3.py
@@ -0,0 +1,12 @@
+for number in range(1, 101):
+    if number == 1:
+        print(str(number) + " процент ")
+    elif number < 5:
+        print(str(number) + " процента")
+    elif number == 41:
+        print(str(number) + " процент ")
+
+    elif number == 41 < 45:
+        print(str(number) + " процента")
+    else:
+        print(str(number) + " процентов")
